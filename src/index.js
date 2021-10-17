4a005d9bb4071d1be55ca0815159340f023c0276
toshko89 <todor_petkov@mail.bg>
2021-10-18 00:36:36 +0300

    set handlebars

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const app = express();
 const PORT = 3000;
 const DB_CONNECTION_STRING = 'mongodb://localhost:27017/cubes';
 
-app.use(express.static(path.resolve(__dirname,'./views')));
 app.use(express.static(path.resolve(__dirname,'./static')));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
@@ -17,9 +16,10 @@ app.engine('hbs',handlebars({
     extname:'hbs'
 }));
 app.set('view engine','hbs');
+app.set('views', path.resolve(__dirname, './views'));
 
 app.get('/',(req,res)=>{
-    
+    res.render('main');
 })
 
 mongoose.connect(DB_CONNECTION_STRING)
