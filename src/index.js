1bd7daca5e9ef8f1c753815fc6dd336544b700c7
toshko89 <todor_petkov@mail.bg>
2021-10-17 23:53:10 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const handlebars = require('express-handlebars');
+
+const app = express();
+const PORT = 3000;
+const DB_CONNECTION_STRING = 'mongodb://localhost:27017/cubes';
+
+app.use(express.static(path.resolve(__dirname,'./views')));
+app.use(express.static(path.resolve(__dirname,'./static')));
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+app.engine('hbs',handlebars({
+    extname:'hbs'
+}));
+app.set('view engine','hbs');
+
+app.get('/',(req,res)=>{
+    
+})
+
+mongoose.connect(DB_CONNECTION_STRING)
+    .then(app.listen(PORT,() => console.log(`Express running on port: ${PORT}...`)))
+    .catch(err=>{
+        console.log(err);
+    })
